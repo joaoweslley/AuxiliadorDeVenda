53ccb8cecbe5ff9297c844acb71729546a57a514
João Weslley Rodrigues Nascimento <joao.nascimento@onlineapp.com.br>
2020-11-30 18:05:00 -0300

    PrimeiroCommit

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AuxiliadorDeVenda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        
+private void button1_Click(object sender, EventArgs e)
+        {
+
+            double ValorC = double.Parse(ValorCompra.Text);
+            double ValorL = 0.1;
+            double ValorV = ValorC + (ValorC * ValorL);
+
+            MessageBox.Show("Voce devera vender esse produto a R$ " + ValorV);
+
+        }
+    }
+}
